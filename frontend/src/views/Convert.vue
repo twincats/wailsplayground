645fe5752951d05d6d9072ee2d18168130365a09
Achul Fauzi <tachibana.natsuru@gmail.com>
2022-10-17 12:24:44 +0700

    design compack (convert)

--- frontend/src/views/Convert.vue
+++ frontend/src/views/Convert.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col h-full">
     <div>
       <div id="tool" class="text-left">
-        <div class="w-full">
+        <div class="w-150">
           <div>Quality:</div>
           <a-slider :default-value="50" />
         </div>
@@ -10,16 +10,16 @@
           <div>Resize :</div>
           <a-slider :default-value="50" />
         </div>
-        <div class="w-130">
+        <div style="flex: 0 0 132px; padding-right: 0">
           <div>Delete :</div>
           <a-space direction="vertical" size="large">
-            <a-radio-group>
+            <a-radio-group size="mini">
               <a-radio value="A">Yes</a-radio>
               <a-radio value="B">No</a-radio>
             </a-radio-group>
           </a-space>
         </div>
-        <div class="w-180">
+        <div style="flex: 0 0 160px; padding-right: 0">
           <div>Convert Engine :</div>
           <a-space direction="vertical" size="large">
             <a-radio-group>
@@ -28,7 +28,7 @@
             </a-radio-group>
           </a-space>
         </div>
-        <div class="w-130">
+        <div style="flex: 0 0 188px; padding-right: 0">
           <div>Format Image :</div>
           <a-space direction="vertical" size="large">
             <a-checkbox-group :default-value="['1']">
@@ -39,25 +39,88 @@
         </div>
       </div>
     </div>
-    <div class="h-10 bg-blue-900">title</div>
-    <div class="h-full bg-gray-900 overflow-auto p-3 text-left">
-      <a-list>
-        <a-list-item v-for="(item, i) in fileManga" :key="i">
-          {{ item }}
-        </a-list-item>
-      </a-list>
+    <div class="h-10 mb-2" style="background-color: var(--color-bg-4)">
+      <a-input
+        v-model="data.title"
+        size="small"
+        :input-attrs="{ class: 'text-center' }"
+        readonly
+      />
     </div>
-    <div class="bg-gray-700">status</div>
+    <div
+      id="mtable"
+      class="h-full overflow-auto p-3 mb-1 text-left"
+      style="background-color: var(--color-bg-4)"
+    >
+      <a-table
+        size="small"
+        v-model:selected-keys="selectedKeys"
+        :show-header="false"
+        :pagination="false"
+        :columns="columnManga"
+        :data="tableManga"
+        @row-click="(c)=>selectManga(c as TableManga)"
+      />
+    </div>
+    <div>
+      <div id="status_convert"></div>
+      <div id="mprogress" class="mt-1">
+        <a-progress
+          status="success"
+          :show-text="false"
+          :stroke-width="20"
+          :percent="0.2"
+        />
+      </div>
+    </div>
+    <div class="text-xs flex justify-between px-3 py-2 h-80px">
+      <div class="status">
+        <div style="min-width: 128px">
+          Size Before : <strong class="text-red-500">1500 MB</strong>
+        </div>
+        <div style="min-width: 117px">
+          Size After : <strong class="text-blue-500">1100 MB</strong>
+        </div>
+        <div style="min-width: 85px">
+          Percent : <strong class="text-green-500">50%</strong>
+        </div>
+      </div>
+      <div class="w-full"></div>
+      <div class="w-full"></div>
+      <div class="bmenu">
+        <a-space>
+          <a-button>Reset</a-button>
+          <a-button>Cover Fix</a-button>
+          <a-button>Save Log</a-button>
+          <a-button type="primary">Convert</a-button>
+        </a-space>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { GetFileManga } from '@wails/go/file/FileReader'
 
-const fileManga = ref<string[]>([])
+interface TableManga {
+  title: string
+}
+const tableManga = reactive<TableManga[]>([])
+const columnManga = [{ title: 'Manga', dataIndex: 'title' }]
 GetFileManga().then(resp => {
-  fileManga.value = resp
+  resp.forEach(item => {
+    tableManga.push({ title: item })
+  })
 })
+
+const selectedKeys = ref([])
+const data = reactive({
+  title: '',
+})
+
+const selectManga = (c: TableManga) => {
+  data.title = c.title
+}
 </script>
 
 <style lang="less" scoped>
@@ -67,12 +130,55 @@ GetFileManga().then(resp => {
   justify-content: space-between;
   > div {
     padding: 0.5rem;
-    // background-color: rgb(31, 41, 55);
+    font-size: 0.75rem;
     display: flex;
     flex-direction: column;
     &:not(:last-child) {
       border-right: 1px solid white;
     }
+    > div {
+      height: 100%;
+    }
   }
+}
+#status_convert {
+  // class="h-25 p-3 overflow-y-scroll text-left"
+  height: 6rem;
+  overflow-y: scroll;
+  text-align: left;
+  padding: 0.75rem;
+  background-color: var(--color-bg-4);
+}
+.status {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  div {
+    text-align: left;
+    align-self: center;
+    padding-right: 0.75rem;
+  }
+}
+
+.bmenu {
+  display: flex;
+}
+
+.arco-radio,
+button,
+.arco-checkbox {
+  font-size: 0.75rem !important;
+}
+</style>
+
+<style>
+#mprogress .arco-progress-line {
+  border-radius: 0 !important;
+}
+#mprogress .arco-progress-line-bar {
+  border-radius: 0 !important;
+}
+#mtable .arco-table-td {
+  font-size: 0.75rem !important;
 }
 </style>
--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,30 +1,3 @@
-export namespace apps {
-	
-	export class Manga {
-	    id: number;
-	    title: string;
-	    status_end: boolean;
-	    mdex: string;
-	    created_at: string;
-	    updated_at: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new Manga(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.id = source["id"];
-	        this.title = source["title"];
-	        this.status_end = source["status_end"];
-	        this.mdex = source["mdex"];
-	        this.created_at = source["created_at"];
-	        this.updated_at = source["updated_at"];
-	    }
-	}
-
-}
-
 export namespace models {
 	
 	export class Manga {
@@ -120,6 +93,33 @@ export namespace models {
 		    }
 		    return a;
 		}
+	}
+
+}
+
+export namespace apps {
+	
+	export class Manga {
+	    id: number;
+	    title: string;
+	    status_end: boolean;
+	    mdex: string;
+	    created_at: string;
+	    updated_at: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Manga(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.title = source["title"];
+	        this.status_end = source["status_end"];
+	        this.mdex = source["mdex"];
+	        this.created_at = source["created_at"];
+	        this.updated_at = source["updated_at"];
+	    }
 	}
 
 }
